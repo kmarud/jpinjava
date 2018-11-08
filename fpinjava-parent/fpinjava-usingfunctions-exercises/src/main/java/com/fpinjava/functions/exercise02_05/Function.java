eff98db6fe6a91153bf93f95f9d2f3d87dd2d27e
Kamil Marud <kamil.marud@globallogic.com>
2018-11-08 15:29:24 +0100

    exercised 1-11

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
@@ -2,9 +2,12 @@ package com.fpinjava.functions.exercise02_05;
 
 public interface Function<T, U> {
 
-  U apply(T arg);
+    U apply(T arg);
 
-  static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
-    throw new RuntimeException("To be implemented.");
-  }
+    static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
+        return arg1 -> (Function<Function<T, U>, Function<T, V>>)
+                arg2 -> (Function<T, V>)
+                        arg3 -> arg1
+                                .apply(arg2.apply(arg3));
+    }
 }
