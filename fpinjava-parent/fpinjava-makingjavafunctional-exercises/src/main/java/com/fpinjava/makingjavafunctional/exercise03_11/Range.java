4cb91a8cb4c29889163905e7f34a08a904c15c73
s200894 <kamil.marud@gmail.com>
2018-11-13 13:52:36 +0100

    Added exercies 9-14 and EmailValidationTest (#2)

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
@@ -1,11 +1,19 @@
 package com.fpinjava.makingjavafunctional.exercise03_11;
 
-import java.util.List;
+import com.fpinjava.common.CollectionUtilities;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Range {
 
-  public static List<Integer> range(int start, int end) {
-    throw new RuntimeException("To be implemented");
-  }
+    public static List<Integer> range(int start, int end) {
+        List<Integer> result = new ArrayList<>();
+        int temp = start;
+        while (temp < end) {
+            result = CollectionUtilities.append(result, temp);
+            temp++;
+        }
+        return result;
+    }
 }
--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_14/Range.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_14/Range.java
@@ -1,11 +1,14 @@
 package com.fpinjava.makingjavafunctional.exercise03_14;
 
-import java.util.List;
+import com.fpinjava.makingjavafunctional.exercise03_13.CollectionUtilities;
 
+import java.util.List;
 
 public class Range {
 
-  public static List<Integer> range(Integer start, Integer end) {
-    throw new RuntimeException("To be implemented");
-  }
+    public static List<Integer> range(Integer start, Integer end) {
+        return end <= start
+                ? CollectionUtilities.list()
+                : CollectionUtilities.prepend(start, range(start + 1, end));
+    }
 }
